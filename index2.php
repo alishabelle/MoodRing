2e71495ec88d6d1e71e2d9a91914654d88cda2cc
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-10-25 12:37:26 -0400

    Update: contans php code needed to speak to the server

---
+++ index2.php
@@ -0,0 +1,40 @@
+
+//getting data from phpMyAdmin
+
+<script>
+	//call ajax
+	var ajax = new XMLHttpRequest();	
+	var method = "GET";
+	var url = "data.php"
+	var asynchronus = true;
+
+	ajax.open(method, url, asynchronus);
+
+	//sending ajax
+	ajax.send();
+
+	//receiving response from data.php
+	ajax.onreadystatechange = function() {
+    if (this.readyState = 4 && this.status 200) {
+				//converting JSON back to array
+				var data = JSON.parse(this.responseText);
+				console.log(data); // for debugging
+
+				//html value for <tbody>
+				var html = "";
+
+				//looping through the data
+				for (var m = 0; m < data.length; m++)
+				{
+					var colorMood = data[m].color_hexcode/color_mood;
+				
+					// append at html
+					html += "<tr>";
+						html +="<td>" + colorMood + "</td>";
+					html += "</tr>";
+				}
+
+				// replaceing the <tbody> of <tabke>
+				document.getElementById("color_hexcode/color_mood").innerHTML = html;
+};
+</script>
